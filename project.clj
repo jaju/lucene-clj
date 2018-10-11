fd9380e230ce249dee9b562b3473237b5870aa83
Ravindra R. Jaju <jaju@msync.org>
2018-10-11 06:02:28 +0530

    Updated clojure version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "https://github.com/jaju/lucene-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta2"]
                  [org.apache.lucene/lucene-core ~lucene-version]
                  [org.apache.lucene/lucene-queryparser ~lucene-version]
                  [org.apache.lucene/lucene-analyzers-common ~lucene-version]
