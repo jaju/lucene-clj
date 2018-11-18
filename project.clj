4fc8b69586694b150376a38a461ebeed9aa51f6c
Ravindra R. Jaju <jaju@msync.org>
2018-11-18 17:48:32 +0530

    Updated version - 0.2.0-SNAPSHOT. Minor dep update

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (def lucene-version "7.5.0")
 
-(defproject org.msync/lucene-clj "0.1.0-SNAPSHOT"
+(defproject org.msync/lucene-clj "0.2.0-SNAPSHOT"
   :description "Lucene bindings for Clojure"
   :url "https://github.com/jaju/lucene-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-beta4"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta6"]
                  [org.apache.lucene/lucene-core ~lucene-version]
                  [org.apache.lucene/lucene-queryparser ~lucene-version]
                  [org.apache.lucene/lucene-analyzers-common ~lucene-version]
