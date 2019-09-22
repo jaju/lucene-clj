672bce6fac28189abf09aee07cc9cc9a3308c455
Ravindra R. Jaju <jaju@msync.org>
2019-09-22 10:17:13 +0530

    [Intermediate] Handle CSV-columns in CSV input, to demonstrate use of contexts in suggestions

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,17 +1,24 @@
+;; [[file:~/github/lucene-clj/README.org::*Preamble][Preamble:1]]
 (ns dev
   (:require [msync.lucene :as lucene]
             [msync.lucene
              [store :as store]
              [document :as ld]
              [tests-common :refer :all]]))
+;; Preamble:1 ends here
 
+;; [[file:~/github/lucene-clj/README.org::*Create%20an%20index][Create an index:1]]
 (def index (store/store :memory :analyzer album-data-analyzer))
+;; Create an index:1 ends here
 
+;; [[file:~/github/lucene-clj/README.org::*Index%20documents%20-%20which%20are%20Clojure%20maps][Index documents - which are Clojure maps:1]]
 (lucene/index! index album-data
-               {:context-fn     #(map clojure.string/trim (clojure.string/split (:Genre %) #","))
+               {:context-fn     :Genre
                 :suggest-fields [:Album :Artist]
                 :stored-fields  [:Number :Year :Album :Artist :Genre :Subgenre]})
+;; Index documents - which are Clojure maps:1 ends here
 
+;; [[file:~/github/lucene-clj/README.org::*Now,%20we%20can%20search][Now, we can search:1]]
 (clojure.pprint/pprint (do (lucene/search index 
                {:Year "1968"} ;; Map of field-values to search with
                {:results-per-page 5 ;; Control the number of results returned
@@ -19,7 +26,12 @@
                 :hit->doc         #(-> %
                                        ld/document->map
                                        (select-keys [:Year :Album]))})))
+;; Now, we can search:1 ends here
 
-(clojure.pprint/pprint (do (lucene/suggest index :Album "par" {:hit->doc ld/document->map :fuzzy? false :contexts ["Electronic"]})))
+;; [[file:~/github/lucene-clj/README.org::*We%20can%20ask%20for%20suggestions%20on%20the%20fields%20indexed%20to%20support%20it][We can ask for suggestions on the fields indexed to support it:1]]
+(clojure.pprint/pprint (do (lucene/suggest index :Album "par" {:hit->doc (fn [d] (ld/document->map d :multi-fields #{:Genre})) :fuzzy? false :contexts ["Electronic"]})))
+;; We can ask for suggestions on the fields indexed to support it:1 ends here
 
+;; [[file:~/github/lucene-clj/README.org::*We%20can%20ask%20for%20suggestions%20on%20the%20fields%20indexed%20to%20support%20it][We can ask for suggestions on the fields indexed to support it:3]]
 (clojure.pprint/pprint (do (lucene/suggest index :Album "per" {:hit->doc ld/document->map :fuzzy? true :contexts ["Electronic"]})))
+;; We can ask for suggestions on the fields indexed to support it:3 ends here
--- test/msync/lucene/tests_common.clj
+++ test/msync/lucene/tests_common.clj
@@ -1,9 +1,20 @@
+;; [[file:~/github/lucene-clj/README.org::*Given][Given:1]]
 (ns msync.lucene.tests-common
   (:require [msync.lucene
              [analyzers :as analyzers]]
             [msync.lucene.utils :as utils]
             [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(defn- process-column [coll column]
+  (assoc coll column
+         (map s/trim (s/split (get coll column) #","))))
+
+(defn process-album-data-row [row]
+  (-> row
+      (process-column :Genre)
+      (process-column :Subgenre)))
 
 (defonce sample-data (-> "sample-data.csv"
                          io/resource
@@ -11,11 +22,12 @@
                          csv/read-csv
                          utils/docs:vecs->maps))
 
-(defonce album-data (-> "albumlist.csv"
-                        io/resource
-                        slurp
-                        csv/read-csv
-                        utils/docs:vecs->maps))
+(def album-data (->> "albumlist.csv"
+                         io/resource
+                         slurp
+                         csv/read-csv
+                         utils/docs:vecs->maps
+                         (map process-album-data-row)))
 
 (defonce default-analyzer (analyzers/standard-analyzer))
 (defonce keyword-analyzer (analyzers/keyword-analyzer))
@@ -26,3 +38,4 @@
                                 {:Year     keyword-analyzer
                                  :Genre    keyword-analyzer
                                  :Subgenre keyword-analyzer}))
+;; Given:1 ends here
