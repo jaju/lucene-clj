2b0be7b512e188e2fb3f4edba8fef09634cafffa
Ravindra R. Jaju <jaju@msync.org>
2019-09-22 22:51:21 +0530

    And the missing bits of the previous commit

--- dev/dev.clj
+++ dev/dev.clj
@@ -32,7 +32,7 @@
 ;; [[file:~/github/lucene-clj/README.org::*Now,%20we%20can%20search][Now, we can search:5]]
 (clojure.pprint/pprint (do (lucene/search index {:Year "1979"}
                {:results-per-page 2
-                :hit->doc #(ld/document->map % :multi-fields #{:Genre :Subgenre})})))
+                :hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])})))
 ;; Now, we can search:5 ends here
 
 ;; [[file:~/github/lucene-clj/README.org::*Now,%20we%20can%20search][Now, we can search:7]]
@@ -47,14 +47,14 @@
 
 ;; [[file:~/github/lucene-clj/README.org::*Suggestions%20support%20for%20fields%20passed%20via%20/:suggest-fields/][Suggestions support for fields passed via /:suggest-fields/:1]]
 (clojure.pprint/pprint (do (lucene/suggest index :Album "par"
-                {:hit->doc #(ld/document->map % :multi-fields #{:Genre :Subgenre})
+                {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
                  :fuzzy? false
                  :contexts ["Electronic"]})))
 ;; Suggestions support for fields passed via /:suggest-fields/:1 ends here
 
 ;; [[file:~/github/lucene-clj/README.org::*Suggestions%20support%20for%20fields%20passed%20via%20/:suggest-fields/][Suggestions support for fields passed via /:suggest-fields/:3]]
 (clojure.pprint/pprint (do (lucene/suggest index :Album "per"
-                {:hit->doc #(ld/document->map % :multi-fields #{:Genre :Subgenre})
+                {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
                  :fuzzy? true
                  :contexts ["Electronic"]})))
 ;; Suggestions support for fields passed via /:suggest-fields/:3 ends here
