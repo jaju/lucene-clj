bb53e6746bf757534fdc5c80a98dfd155c268bf3
Ravindra R. Jaju <jaju@msync.org>
2020-08-18 18:43:50 +0530

    Fixed broken indentation

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,9 +1,9 @@
 ;; [[file:../README.org::*Preamble][Preamble:1]]
 (ns dev
-    (:require [msync.lucene :as lucene]
-              [msync.lucene
-               [document :as ld]
-               [tests-common :as common]]))
+  (:require [msync.lucene :as lucene]
+            [msync.lucene
+             [document :as ld]
+             [tests-common :as common]]))
 ;; Preamble:1 ends here
 
 ;; [[file:../README.org::*Create an index][Create an index:1]]
@@ -13,48 +13,48 @@
 
 ;; [[file:../README.org::*Index documents][Index documents:1]]
 (lucene/index! album-index common/album-data
-                 {:stored-fields  [:Number :Year :Album :Artist :Genre :Subgenre]
-                  :suggest-fields [:Album :Artist]
-                  :context-fn     :Genre})
+               {:stored-fields  [:Number :Year :Album :Artist :Genre :Subgenre]
+                :suggest-fields [:Album :Artist]
+                :context-fn     :Genre})
 ;; Index documents:1 ends here
 
 ;; [[file:../README.org::*Now, we can search][Now, we can search:1]]
 (clojure.pprint/pprint (do (lucene/search album-index {:Year "1979"}
-                 {:results-per-page 2})))
+               {:results-per-page 2})))
 ;; Now, we can search:1 ends here
 
 ;; [[file:../README.org::*Now, we can search][Now, we can search:3]]
 (clojure.pprint/pprint (do (lucene/search album-index {:Year "1979"}
-                 {:results-per-page 2
-                  :hit->doc ld/document->map})))
+               {:results-per-page 2
+                :hit->doc ld/document->map})))
 ;; Now, we can search:3 ends here
 
 ;; [[file:../README.org::*Now, we can search][Now, we can search:5]]
 (clojure.pprint/pprint (do (lucene/search album-index
-                 {:Year "1979"}
-                 {:results-per-page 2
-                  :hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])})))
+               {:Year "1979"}
+               {:results-per-page 2
+                :hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])})))
 ;; Now, we can search:5 ends here
 
 ;; [[file:../README.org::*Now, we can search][Now, we can search:7]]
-(clojure.pprint/pprint (do (lucene/search album-index 
-                 {:Year "1968"} ;; Map of field-values to search with
-                 {:results-per-page 5 ;; Control the number of results returned
-                  :page 4             ;; Page number, starting 0 as default
-                  :hit->doc         #(-> %
-                                         ld/document->map
-                                         (select-keys [:Year :Album]))})))
+(clojure.pprint/pprint (do (lucene/search album-index
+               {:Year "1968"} ;; Map of field-values to search with
+               {:results-per-page 5 ;; Control the number of results returned
+                :page 4             ;; Page number, starting 0 as default
+                :hit->doc         #(-> %
+                                       ld/document->map
+                                       (select-keys [:Year :Album]))})))
 ;; Now, we can search:7 ends here
 
 ;; [[file:../README.org::*Suggestions support for fields passed via ~:suggest-fields~][Suggestions support for fields passed via ~:suggest-fields~:1]]
 (clojure.pprint/pprint (do (lucene/suggest album-index :Album "par"
-                  {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
-                   :contexts ["Electronic"]})))
+                {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
+                 :contexts ["Electronic"]})))
 ;; Suggestions support for fields passed via ~:suggest-fields~:1 ends here
 
 ;; [[file:../README.org::*Suggestions support for fields passed via ~:suggest-fields~][Suggestions support for fields passed via ~:suggest-fields~:3]]
 (clojure.pprint/pprint (do (lucene/suggest album-index :Album "per"
-                  {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
-                   :fuzzy? true
-                   :contexts ["Electronic"]})))
+                {:hit->doc #(ld/document->map % :multi-fields [:Genre :Subgenre])
+                 :fuzzy? true
+                 :contexts ["Electronic"]})))
 ;; Suggestions support for fields passed via ~:suggest-fields~:3 ends here
--- test/msync/lucene/tests_common.clj
+++ test/msync/lucene/tests_common.clj
@@ -1,47 +1,47 @@
 ;; [[file:../../../README.org::*Some Background - Data Preparation][Some Background - Data Preparation:2]]
 (ns msync.lucene.tests-common
-    (:require [msync.lucene
-               [analyzers :as analyzers]
-               [document :as ld]]
-              [clojure.data.csv :as csv]
-              [clojure.java.io :as io]
-              [clojure.string :as s]))
+  (:require [msync.lucene
+             [analyzers :as analyzers]
+             [document :as ld]]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as s]))
 
-  (defn read-csv-resource-file
-    "Locate a file on the resource path and parse it as CSV,
-    creating a sequence of rows - each row being a list of the
-    CSV column-values"
-    [filename]
-    (-> filename
-        io/resource
-        slurp
-        csv/read-csv))
-
-
-  (defonce sample-data-file "sample-data.csv")
-  (defonce albums-file "albumlist.csv")
+(defn read-csv-resource-file
+  "Locate a file on the resource path and parse it as CSV,
+  creating a sequence of rows - each row being a list of the
+  CSV column-values"
+  [filename]
+  (-> filename
+      io/resource
+      slurp
+      csv/read-csv))
 
 
-  (defonce sample-data (-> sample-data-file
-                           read-csv-resource-file
-                           ld/vecs->maps))
+(defonce sample-data-file "sample-data.csv")
+(defonce albums-file "albumlist.csv")
+
+
+(defonce sample-data (-> sample-data-file
+                         read-csv-resource-file
+                         ld/vecs->maps))
 
 
 
-  (defn- process-csv-column [coll column]
-    (assoc coll column
-           (map s/trim (s/split (get coll column) #","))))
+(defn- process-csv-column [coll column]
+  (assoc coll column
+         (map s/trim (s/split (get coll column) #","))))
 
 
-  (defn process-album-data-row [row]
-    (-> row
-        (process-csv-column :Genre)
-        (process-csv-column :Subgenre)))
+(defn process-album-data-row [row]
+  (-> row
+      (process-csv-column :Genre)
+      (process-csv-column :Subgenre)))
 
-  (defonce album-data (->> albums-file
-                           read-csv-resource-file
-                           ld/vecs->maps
-                           (map process-album-data-row)))
+(defonce album-data (->> albums-file
+                         read-csv-resource-file
+                         ld/vecs->maps
+                         (map process-album-data-row)))
 ;; Some Background - Data Preparation:2 ends here
 
 ;; [[file:../../../README.org::*Creating Analyzers][Creating Analyzers:1]]
@@ -49,13 +49,13 @@
 
 
 
-  (defonce keyword-analyzer (analyzers/keyword-analyzer))
+(defonce keyword-analyzer (analyzers/keyword-analyzer))
 
 
 
-  (defonce album-data-analyzer
-    (analyzers/per-field-analyzer default-analyzer
-                                  {:Year     keyword-analyzer
-                                   :Genre    keyword-analyzer
-                                   :Subgenre keyword-analyzer}))
+(defonce album-data-analyzer
+  (analyzers/per-field-analyzer default-analyzer
+                                {:Year     keyword-analyzer
+                                 :Genre    keyword-analyzer
+                                 :Subgenre keyword-analyzer}))
 ;; Creating Analyzers:1 ends here
